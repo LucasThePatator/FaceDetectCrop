0e12702d1d7b5ee4dbd398e8fc44b5e2024722bf
m-btt <dev@a.frings.space>
2023-07-23 20:07:09 +0200

    Labels table already existing no longer causes failure.

--- settings/create_database.py
+++ settings/create_database.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 conn = sqlite3.connect('labels.db')
-query = (''' CREATE TABLE LABELS
+query = (''' CREATE TABLE IF NOT EXISTS LABELS
             (FOLDER           TEXT    NOT NULL,
             NAME        CHAR(50) NOT NULL
             );''')
